d94acf11a4cbb3655c5abf5e17a2297192266f73
vicmmoraM <vicmmora@espol.edu.ec>
2024-11-26 01:21:50 -0500

    ActividadFisica Completa

--- demo/src/main/java/com/example/controlador/ActividadFisicaControlador.java
+++ demo/src/main/java/com/example/controlador/ActividadFisicaControlador.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.example.controlador;
-
+import java.text.SimpleDateFormat;
+import java.util.*;
 /**
  *
  * @author Victor
  */
+
+import com.example.modelo.ActividadFisica;
 public class ActividadFisicaControlador {
-    
+    private List<ActividadFisica> lstacActividadFisicas;
+
+    public ActividadFisicaControlador(){
+        this.lstacActividadFisicas = new ArrayList<>(); //Inicializar lista
+
+        ordenarActividadesPorFecha();
+    }
+
+    public void agregarActividad(ActividadFisica af){
+        lstacActividadFisicas.add(af);
+    }
+
+    private void ordenarActividadesPorFecha(){
+        lstacActividadFisicas.sort((a1, a2) -> a2.getFecha().compareTo(a1.getFecha())); 
+    }
+
+    public List<ActividadFisica> getActividadesOrdenadas() {
+        return lstacActividadFisicas;
+    }
 }
