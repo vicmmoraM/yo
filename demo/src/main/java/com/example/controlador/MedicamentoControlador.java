20b5613dba4acefef8f7d9f2474d23b0050f4b4f
vicmmoraM <vicmmora@espol.edu.ec>
2024-11-26 23:46:09 -0500

    MedicamentoControlador

---
+++ demo/src/main/java/com/example/controlador/MedicamentoControlador.java
@@ -0,0 +1,45 @@
+package com.example.controlador;
+
+import java.util.*;
+
+import com.example.modelo.Medicamento;
+
+public class MedicamentoControlador {
+    private ArrayList<Medicamento> medicamentos;
+
+    public MedicamentoControlador() {
+        medicamentos = new ArrayList<>();
+    }
+
+    public void agregarMedicamento(String nombre, String presentacion, int cantidad, String frecuencia, List<String> horasToma, int dosis) {
+        medicamentos.add(new Medicamento(nombre, presentacion, cantidad, frecuencia, horasToma, dosis));
+        System.out.println("Medicamento añadido exitosamente.");
+    }
+
+    public List<Medicamento> listarMedicamentos() {
+        return medicamentos;
+    }
+
+    public boolean eliminarMedicamento(int id) {
+        for (Medicamento medicamento : medicamentos) {
+            if (medicamento.getId() == id) {
+                medicamentos.remove(medicamento);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean registrarToma(int id, int cantidadTomada) {
+        for (Medicamento medicamento : medicamentos) {
+            if (medicamento.getId() == id) {
+                if (medicamento.getCantidad() >= cantidadTomada) {
+                    medicamento.setCantidad(medicamento.getCantidad() - cantidadTomada);
+                    return true;
+                }
+                return false; // No hay suficiente cantidad disponible
+            }
+        }
+        return false; // Medicamento no encontrado
+    }
+}
